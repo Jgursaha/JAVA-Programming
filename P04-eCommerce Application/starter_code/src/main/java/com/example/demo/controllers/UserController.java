28620188a5150f36216aa23c2228aae493edf390
Jitesh Gursahani <jgursahani@gmail.com>
2020-10-05 21:12:23 +1100

    Added Slf4j-log4j binding dependency. Posting in forum to ask how to write and view log files.

---
+++ P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,73 @@
+package com.example.demo.controllers;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private CartRepository cartRepository;
+
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@GetMapping("/id/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		log.info("Received get request");
+		return ResponseEntity.of(userRepository.findById(id));
+	}
+	
+	@GetMapping("/{username}")
+	public ResponseEntity<User> findByUserName(@PathVariable String username) {
+		log.info("Received get request for username: "+username);
+		User user = userRepository.findByUsername(username);
+		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+		User user = new User();
+		user.setUsername(createUserRequest.getUsername());
+		log.info("User name set with ", createUserRequest.getUsername());
+		Cart cart = new Cart();
+
+		if(createUserRequest.getPassword().length() < 7 ||
+			!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+			log.error("Error with user password. Cannot create user{}", createUserRequest.getUsername());
+			return ResponseEntity.badRequest().build();
+		}
+
+		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
+
+		user.setCart(cart);
+		userRepository.save(user);
+		cartRepository.save(cart);
+		return ResponseEntity.ok(user);
+	}
+	
+}
---
+++ P04-eCommerce Application/starter_code/src/main/java/com/example/demo/model/persistence/User.java
@@ -0,0 +1,65 @@
+package com.example.demo.model.persistence;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+@Entity
+@Table(name = "user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonProperty
+	private long id;
+	
+	@Column(nullable = false, unique = true)
+	@JsonProperty
+	private String username;
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@Column(nullable = false)
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id", referencedColumnName = "id")
+	@JsonIgnore
+    private Cart cart;
+	
+	public Cart getCart() {
+		return cart;
+	}
+
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+	
+}
